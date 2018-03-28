ad11d799ae3e1eb4a6d6c1be6d22c0865599a71c
Taowy <921664773@qq.com>
2018-03-28 19:52:30 +0800

    details

--- pages/posterwall/details/details.js
+++ pages/posterwall/details/details.js
@@ -6,13 +6,38 @@ Page({
      * 页面的初始数据
      */
     data: {
-        currentPoster: null,
+        // currentPoster: null,
         statusText: [
             "报名该活动",
             "您已报名",
             "报名人数已达上限",
             "已过报名截止时间"
-        ]
+        ],
+        currentPoster: {
+          coverImageUrl: '../../../image/sysu-icon.jpeg',
+          name: '第二届毽球团体赛+趣味挑战赛',
+          tag: [
+            '体育',
+            '东校区'
+          ],
+          startTime: '5月25日19:00-21:00',
+          location: '体育馆羽毛球场',
+          enrollCondition: '全体在校生',
+          sponsor: '中东毽球协会',
+
+          // line
+          introduction: '本项赛事是由中东毽球协会主办的趣味体育赛事，又好玩又有趣，参加即有体育章，欢迎大家踊跃报名',
+          detail: '欢迎扫描下方二维码，关注“中东毽球协会”公众号了解活动详情。',
+          QRcode: '../../../image/QRcode.png',
+
+          // line
+          reward: '参与即有5个体育章',
+          requirement: '团体赛限3人组队，可加1人替补，限16队；趣味单人赛可以参与，需完成两个挑战，限100人。',
+          enrollWay: '步骤一：打开网站（http://actplus.sysuactivity.com/X）或在“活动plus”小程序找到改活动进入报名界面。\n' + '步骤二：填写报名信息并提交。',
+          enrollEndTime: '5月23日 23:59'
+        }
+        // test data
+
     },
     enrollButtonTap: function(e) {
         console.log(e);
