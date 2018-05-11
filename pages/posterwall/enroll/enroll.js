4d62cdd7f662024c8c12c58ce98d7b5c13a5fe32
Taowy <921664773@qq.com>
2018-05-11 17:12:20 +0800

    wxml 修改布局

--- pages/posterwall/enroll/enroll.js
+++ pages/posterwall/enroll/enroll.js
@@ -179,13 +179,14 @@ Page({
      * 生命周期函数--监听页面加载
      */
   onLoad: function(options) {
-    // console.log(options);
+    console.log(options);
     var that = this;
     app.getPosterById(options.posterId,
       function(thePoster) {
         that.setData({
           currentPoster: thePoster
         });
+        console.log(thePoster)
       },
       function() {
         console.log(errMsg);
